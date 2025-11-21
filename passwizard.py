fc697c1e7c5078d9cd32de223ec45fd67e8616c6
ealg935 <ealejandrolg@gmail.com>
2025-11-21 07:59:58 -0600

    Update passwizard.py

--- passwizard.py
+++ passwizard.py
@@ -1,18 +1,21 @@
+import random
+
 def contra(longitud):
     caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
     contrasena = ""
-    indice_base = len(caracteres) // 2
+
     for i in range(longitud):
-        indice_cal = (indice_base + i * longitud) % len(caracteres)
-        
-        contrasena = contrasena + caracteres[indice_cal]
-        
+        contrasena += random.choice(caracteres)
+
     return contrasena
+
+
 try:
     longitud_u = int(input("Ingresa un numero de caracteres: "))
     if longitud_u <= 0 or longitud_u > 70:
         print("Longitud no válida")
         longitud_u = 12
+
     contrasena_generada = contra(longitud_u)
     print("\nContraseña generada")
     print(f"Longitud: {longitud_u}")
@@ -20,3 +23,4 @@ try:
 
 except ValueError:
     print("Por favor, ingresa un número válido para la longitud.")
+    
